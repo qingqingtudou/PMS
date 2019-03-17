a7ad1de9c399195ced9aeae08ccc0dd301b57fcf
yexianzhu <aizhule@163.com>
2019-03-17 16:40:36 +0800

    添加service层

---
+++ PMS.Services/BaseService.cs
@@ -0,0 +1,43 @@
+﻿using PMS.Repository.Core;
+using PMS.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMS.Services
+{
+    public class BaseService<T> where T : Entity
+    {
+        /// <summary>
+        /// 用于事务操作
+        /// </summary>
+        /// <value>The unit work.</value>
+        protected IUnitWork UnitWork;
+
+        public BaseService(IUnitWork unitWork, IRepository<T> repository)
+        {
+            UnitWork = unitWork;
+            Repository = repository;
+        }
+
+        /// <summary>
+        /// 用于普通的数据库操作
+        /// </summary>
+        /// <value>The repository.</value>
+        protected IRepository<T> Repository;
+
+        /// <summary>
+        /// 按id批量删除
+        /// </summary>
+        /// <param name="ids"></param>
+        //public void Delete(string[] ids)
+        //{
+        //    Repository.Delete(u => ids.Contains(u.Id));
+        //}
+
+        public T Get(int id)
+        {
+            return Repository.FindSingle(u => u.Id == id);
+        }
+    }
+}
---
+++ PMS.Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using PMS.Infrastructure.Enums;
+using PMS.Infrastructure.Response;
+using PMS.Infrastructure.ViewModel;
+using PMS.Repository.Domain;
+using PMS.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMS.Services
+{
+    public class UserService : BaseService<SysUser>
+    {
+        public UserService(IUnitWork unitWork, IRepository<SysUser> repository) : base(unitWork, repository)
+        {
+        }
+
+        public Response<LoginResult> UserLogin(LoginModel model)
+        {
+            var user = Repository.FindSingle(f => f.Account == model.Account && f.Password == model.Password && f.IsDelete == false && f.Status == (int)EDataStatus.valid);
+
+            Response<LoginResult> result = new Response<LoginResult>();
+
+            if (user == null)
+            {
+                result.Code = (int)EResponse.NotFound;
+                result.Message = "用户名或密码错误";
+                return result;
+            }
+            result.Code = (int)EResponse.Ok;
+            result.Message = "登录成功";
+            result.Payload = new LoginResult()
+            {
+                Name = user.Name
+            };
+            return result;
+        }
+    }
+}
