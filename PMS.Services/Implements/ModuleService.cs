7d550c1d3d40d93577675816e3b1b2b42fd5dc86
yexianzhu <aizhule@163.com>
2019-03-18 21:48:46 +0800

    添加功能模块服务

---
+++ PMS.Services/Implements/ModuleService.cs
@@ -0,0 +1,25 @@
+﻿using PMS.Infrastructure.ViewModel;
+using PMS.Repository;
+using PMS.Repository.Domain;
+using PMS.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMS.Services.Implements
+{
+    /// <summary>
+    /// 功能模块服务
+    /// </summary>
+    public class ModuleService : BaseRepository<Module>, IModuleService
+    {
+        public ModuleService(PMSDbContext context) : base(context)
+        {
+        }
+
+        public List<ModuleView> GetRoleModules()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PMS.Services/Interfaces/IModuleService.cs
@@ -0,0 +1,19 @@
+﻿using PMS.Infrastructure.ViewModel;
+using PMS.Repository.Domain;
+using PMS.Repository.Interfaces;
+using System.Collections.Generic;
+
+namespace PMS.Services.Interfaces
+{
+    /// <summary>
+    /// 功能模块服务接口
+    /// </summary>
+    public interface IModuleService: IRepository<Module>
+    {
+        /// <summary>
+        /// 获取角色功能模块
+        /// </summary>
+        /// <returns></returns>
+        List<ModuleView> GetRoleModules();
+    }
+}
