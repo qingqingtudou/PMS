f3cba7c2a8c6ec8971a3e484dccf633facc675b4
yexianzhu <aizhule@163.com>
2019-03-18 09:30:35 +0800

    添加service引用

--- PMS/Controllers/LoginController.cs
+++ PMS/Controllers/LoginController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PMS.Infrastructure.ViewModel;
+using PMS.Services;
 
 namespace PMS.Controllers
 {
     public class LoginController : Controller
     {
+        private UserService _userService;
+        public LoginController(UserService userService)
+        {
+            _userService = userService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            var result = _userService.UserLogin(model);
+            return Json(result);
         }
     }
 }
