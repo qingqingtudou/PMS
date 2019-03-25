6acfc0a01cadb27b69d06f093bcc07034925c0b3
yexianzhu <aizhule@163.com>
2019-03-25 15:31:55 +0800

    添加角色服务

---
+++ PMS.Services/Implements/RoleService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMS.Infrastructure.Response;
+using PMS.Repository;
+using PMS.Repository.Domain;
+using PMS.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PMS.Services.Implements
+{
+    public class RoleService : BaseRepository<Role>, IRoleService
+    {
+        public RoleService(PMSDbContext context) : base(context)
+        {
+        }
+
+        public List<RoleView> GetUserRoles(int userRole)
+        {
+            var role = _context.Roles.AsNoTracking().Where(w => w.Id == userRole).FirstOrDefault();
+            var query = _context.Roles.AsNoTracking().Where(w => w.Sort > role.Sort).Select(s => new RoleView()
+            {
+                Id = s.Id,
+                Name = s.Name
+            }).ToList();
+            return query;
+        }
+    }
+}
---
+++ PMS.Services/Interfaces/IRoleService.cs
@@ -0,0 +1,19 @@
+﻿using PMS.Infrastructure.Response;
+using PMS.Repository.Domain;
+using PMS.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMS.Services.Interfaces
+{
+    public interface IRoleService:IRepository<Role>
+    {
+        /// <summary>
+        /// 获取用户可见的角色列表
+        /// </summary>
+        /// <param name="userRole">用户角色id</param>
+        /// <returns></returns>
+        List<RoleView> GetUserRoles(int userRole);
+    }
+}
